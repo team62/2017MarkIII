08ece076cd5d49384262c9349ea1b0f3ecc7130a
Jonathan Damico <jj_damico@yahoo.com>
2016-11-24 07:38:12 -0800

    init commit

---
+++ 2017MarkIII.c
@@ -0,0 +1,16 @@
+
+#pragma platform(VEX2)
+#pragma competitionControl(Competition)
+#include "Vex_Competition_Includes.c"
+
+void pre_auton () {
+
+}
+
+task autonomous() {
+
+}
+
+task usercontrol() {
+
+}
